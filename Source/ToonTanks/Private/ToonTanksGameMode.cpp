8b4641e067b49a7c172f884df07e6242f6ee53ea
Benedikt Schnatterbeck <benediktms@hey.com>
2023-08-05 15:22:49 +0100

    Implement custom game mode

---
+++ Source/ToonTanks/Private/ToonTanksGameMode.cpp
@@ -0,0 +1,5 @@
+// This game is the result of a course taken from GameDev.tv. Any quereis about copy right should be taken up with them
+
+
+#include "ToonTanksGameMode.h"
+
---
+++ Source/ToonTanks/Public/ToonTanksGameMode.h
@@ -0,0 +1,17 @@
+// This game is the result of a course taken from GameDev.tv. Any quereis about copy right should be taken up with them
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ToonTanksGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API AToonTanksGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
